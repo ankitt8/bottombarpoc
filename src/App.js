6416069436e2bd5ee1c7d4a865b3562b0dcfdb7a
Ankit Tiwari <ankit.tiwari5@flipkarthealthplus.com>
2024-12-27 11:27:11 +0530

    fix back button handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './styles/BottomBar.css';
 import { browserHistory } from 'react-router';
 import NavigationHistory from "./NavigationHistory.jsx";
@@ -38,29 +38,73 @@ const styles = {
     },
 }
 
+
+// function popstateHandler() {
+//     window.addEventListener("popstate", function (event) {
+//         // find a workaround to know below is triggered only for back event
+//         const currentTabEntries = navigationHistory[currentTab];
+//         if(currentTabEntries.length === 1) {
+//             // if current is home tab then exit the webapp
+//             // if current is not home tab, take user to previous tab journey
+//             return;
+//         }
+//         navigationHistory[currentTab].pop();
+//         const nextPath  = navigationHistory[currentTab][navigationHistory[currentTab].length - 1];
+//         browserHistory.push(nextPath);
+//         // history.pushState({path: nextPath}, '', nextPath);
+//         // trying since pointer is not moving to next state after doing history.pushState
+//         // history.go(1);
+//     });
+// }
+
+
 const App = ({ children }) => {
     const [navigationHistory, setNavigationHistory] = useState({
         home: ['/'],
         categories: ['/categories'],
         cart: ['/cart'],
     });
+    const navigationHistoryRef = useRef(navigationHistory);
+    console.log(navigationHistory);
     useEffect(() => {
+        popstateHandler()
+    }, []);
+    useEffect(() => {
+        navigationHistoryRef.current = navigationHistory;
+    }, [navigationHistory]);
+    function popstateHandler() {
         window.addEventListener("popstate", function (event) {
+            const currentTabRefValue = currentTabRef.current;
+            const navigationHistoryRefValue = navigationHistoryRef.current;
             // find a workaround to know below is triggered only for back event
-            const currentTabEntries = navigationHistory[currentTab];
+            const currentTabEntries = navigationHistoryRefValue[currentTabRefValue];
             if(currentTabEntries.length === 1) {
                 // if current is home tab then exit the webapp
                 // if current is not home tab, take user to previous tab journey
                 return;
             }
-            navigationHistory[currentTab].pop();
-            const nextPath  = navigationHistory[currentTab][navigationHistory[currentTab].length - 1];
+            // navigationHistory[currentTab].pop();
+            setNavigationHistory((prev) => {
+                return {
+                    ...prev,
+                    currentTab: navigationHistoryRefValue[currentTabRefValue].slice(0,-1)
+                }
+            });
+            const nextPath  = navigationHistoryRefValue[currentTabRefValue][navigationHistoryRefValue[currentTabRefValue].length - 2];
             browserHistory.push(nextPath);
+            // history.pushState({path: nextPath}, '', nextPath);
             // trying since pointer is not moving to next state after doing history.pushState
-            history.go(1);
-        })
-    }, []);
+            // history.go(1);
+        });
+    }
+
     const [currentTab, setCurrentTab] = useState('home');
+    const currentTabRef = useRef(currentTab);
+
+    useEffect(() => {
+        currentTabRef.current = currentTab;
+    }, [currentTab]);
+
     function updateNavigationHistory(tab, path, action = 'add') {
         if (!tab) return;
         console.log(action)
@@ -91,6 +135,7 @@ const App = ({ children }) => {
 
             // Replace the browser history to the last visited page of the target tab
             browserHistory.replace(lastVisitedPageOfTargetTab);
+            // history.replaceState({ path: lastVisitedPageOfTargetTab }, '', lastVisitedPageOfTargetTab);
             setCurrentTab(targetTab);
             return;
         }
@@ -99,6 +144,7 @@ const App = ({ children }) => {
 
         // Push the new path to the browser history
         browserHistory.replace(path);
+        // history.replaceState({ path: path }, '', path);
         updateNavigationHistory(targetTab, path, 'add');
     };
 
@@ -108,7 +154,7 @@ const App = ({ children }) => {
             <>
                 <div>{children && React.cloneElement(children, {navigate})}</div>
                 <h3 className="header">Browsers navigation history entries</h3>
-                <NavigationEntries/>
+                <NavigationEntries />
                 <NavigationHistory navigationHistory={navigationHistory}/>
             </>
             <nav style={styles.navBar}>

52704de8e76103e272b41fa4024cb7aa9684b1db
Ankit Tiwari <ankit.tiwari5@flipkarthealthplus.com>
2025-01-07 14:19:37 +0530

    feat: added document title

--- src/Routes.js
+++ src/Routes.js
@@ -1,45 +1,63 @@
 import {browserHistory, Router, Route, IndexRoute} from "react-router";
-import React from "react";
+import React, {useEffect} from "react";
 import App from "./App";
 import './style.css';
 
-const Home = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const Home = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Home L0';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Home Tab</div>
         <button onClick={() => navigate('/l1')}>Go to L1</button>
     </div>
-);
+};
 
-const HomeL1 = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const HomeL1 = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Home L1';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Home - L1 Page</div>
         <button onClick={() => navigate('/l2')}>Go to L2</button>
     </div>
-);
+};
 
-const HomeL2 = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const HomeL2 = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Home L2';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Home - L2 Page</div>
         <button onClick={() => navigate('/l3')}>Go to L3</button>
     </div>
-);
+};
 
-const HomeL3 = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const HomeL3 = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Home L3';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Home - L3 Page</div>
         {/*<button onClick={() => navigate('/')}>Go to Home</button>*/}
     </div>
-);
+};
 
-const Categories = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const Categories = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Categories L0';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Categories Tab</div>
         <button onClick={() => navigate('/categories/l1')}>Go to L1</button>
     </div>
-);
+};
 
-const CategoriesL1 = ({navigate}) => (
-    <div className="pageHeadingContainer">
+const CategoriesL1 = ({navigate}) => {
+    useEffect(() => {
+        document.title = 'Categories L1';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Categories - L1 Page</div>
         <div style={{display: 'flex'}}>
             <button onClick={() => navigate('/categories/l2')}>Go to L2</button>
@@ -48,43 +66,59 @@ const CategoriesL1 = ({navigate}) => (
         </div>
 
     </div>
-);
+};
 
-const CategoriesL2 = ({navigate}) => (
-    <div className="pageHeadingContainer">
+const CategoriesL2 = ({navigate}) => {
+    useEffect(() => {
+        document.title = 'Categories L2';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Categories - L2 Page</div>
         {/*<button onClick={() => navigate('/categories')}>Go to Categories</button>*/}
     </div>
-);
+};
 
 
-const CategoriesL3 = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const CategoriesL3 = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Categories L3';
+    }, []);
+
+    return <div className="pageHeadingContainer">
         <div className="heading">Categories - L3 Page</div>
         {/*<button onClick={() => navigate('/categories')}>Go to Categories</button>*/}
     </div>
-);
+};
 
-const Cart = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const Cart = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Cart L0';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Cart Tab</div>
         <button onClick={() => navigate('/cart/l1')}>Go to L1</button>
     </div>
-);
+};
 
-const CartL1 = ({navigate}) => (
-    <div className="pageHeadingContainer">
+const CartL1 = ({navigate}) => {
+    useEffect(() => {
+        document.title = 'Cart L1';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Cart - L1 Page</div>
         <button onClick={() => navigate('/cart/l2')}>Go to L2</button>
     </div>
-);
+};
 
-const CartL2 = ({ navigate }) => (
-    <div className="pageHeadingContainer">
+const CartL2 = ({ navigate }) => {
+    useEffect(() => {
+        document.title = 'Cart L2';
+    }, []);
+    return <div className="pageHeadingContainer">
         <div className="heading">Cart - L2 Page</div>
         {/*<button onClick={() => navigate('/cart')}>Go to Cart</button>*/}
     </div>
-);
+};
 
 const NotFound = () => <div>404 - Page Not Found</div>;
 
